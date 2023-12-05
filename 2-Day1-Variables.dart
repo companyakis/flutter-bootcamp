d5201c82f1beae8df6df904acba327ff1a0715ef
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-05 10:43:37 +0300

    Create 2-Day1-Variables.dart

---
+++ 2-Day1-Variables.dart
@@ -0,0 +1,22 @@
+void main() {
+  var myName = "Mustafa Buyukdereli"; //double quote
+  
+  print(myName);
+  
+  int aNumber = 2023;
+  
+  String aProverb = 'A rolling stone gathers no moss.'; //single quote
+  
+  double priceThatProduct = 32.29;
+  
+  bool isCompleted = false;
+  
+  // $ and print
+  
+  int x = 303;
+  double y = 303.303;
+  
+  print("x + y = ${x + y}"); //x + y = 606.303
+
+}
+  
