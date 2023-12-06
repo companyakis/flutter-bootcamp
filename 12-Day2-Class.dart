ed7b8e309222453d4b7da6ee8b4e4e74fab034fa
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-06 19:33:57 +0300

    Create 12-Day2-Class.dart

---
+++ 12-Day2-Class.dart
@@ -0,0 +1,10 @@
+//country.dart file
+
+class Country {
+  String name; //late String
+  int population; //late int
+  bool isRepublic; //late bool
+
+  //constructor
+  Country(this.name, this.population, this.isRepublic);
+}
