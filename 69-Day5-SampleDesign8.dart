15b3284961e27c101ddb76b9aaa2f22b6a78f09b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-13 15:59:50 +0300

    Create 69-Day5-SampleDesign8.dart

---
+++ 69-Day5-SampleDesign8.dart
@@ -0,0 +1,50 @@
+//myworks.dart file
+
+//main.dart is different
+
+import 'package:design_steps/colors.dart';
+import 'package:flutter/material.dart';
+
+class MyWorks extends StatefulWidget {
+  const MyWorks({super.key});
+
+  @override
+  State<MyWorks> createState() => _MyWorksState();
+}
+
+class _MyWorksState extends State<MyWorks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Pizza",
+          style: TextStyle(
+            color: fontColor1, //from colors.dart file
+            fontFamily: "Pacifico",
+            fontSize: 20,
+          ),
+        ),
+        backgroundColor: mainColor, //from colors.dart file
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 12,
+            ),
+            child: Text(
+              "Beef Cheese",
+              style: TextStyle(
+                fontSize: 30,
+                color: mainColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
