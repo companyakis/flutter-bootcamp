91f4964d09fa56c7af45a28bd9f11be88343a27f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-13 16:45:55 +0300

    Create 74-Day5-SampleDesign13.dart

---
+++ 74-Day5-SampleDesign13.dart
@@ -0,0 +1,94 @@
+//myworks.dart file
+
+//main.dart is different
+
+import 'package:design_steps/colors.dart';
+import 'package:flutter/material.dart';
+
+class MyWorks extends StatefulWidget {
+  const MyWorks({super.key});
+
+  @override
+  State<MyWorks> createState() => _MyWorksState();
+}
+
+class _MyWorksState extends State<MyWorks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Pizza",
+          style: TextStyle(
+            color: fontColor1, //from colors.dart file
+            fontFamily: "Pacifico",
+            fontSize: 20,
+          ),
+        ),
+        backgroundColor: mainColor, //from colors.dart file
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 12,
+            ),
+            child: Text(
+              "Beef Cheese",
+              style: TextStyle(
+                fontSize: 30,
+                color: mainColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Image.asset("pictures/pizza_resim.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Cheese",
+                    style: TextStyle(color: fontColor1),
+                  ),
+                  style: TextButton.styleFrom(backgroundColor: mainColor),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Sausage",
+                    style: TextStyle(color: fontColor1),
+                  ),
+                  style: TextButton.styleFrom(backgroundColor: mainColor),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Olive",
+                    style: TextStyle(color: fontColor1),
+                  ),
+                  style: TextButton.styleFrom(backgroundColor: mainColor),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Pepper",
+                    style: TextStyle(color: fontColor1),
+                  ),
+                  style: TextButton.styleFrom(backgroundColor: mainColor),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
