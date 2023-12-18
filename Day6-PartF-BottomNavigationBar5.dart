ebaa97a1ef6566b124dc3a70dca1776585106713
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-18 22:47:11 +0300

    Create Day6-PartF-BottomNavigationBar5.dart

---
+++ Day6-PartF-BottomNavigationBar5.dart
@@ -0,0 +1,48 @@
+//bottom_navigation.dart file
+
+import 'package:daysix/page_one.dart';
+import 'package:daysix/page_three.dart';
+import 'package:daysix/page_two.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigationPage extends StatefulWidget {
+  const BottomNavigationPage({super.key});
+  @override
+  State<BottomNavigationPage> createState() => _BottomNavigationPageState();
+}
+
+class _BottomNavigationPageState extends State<BottomNavigationPage> {
+  int selectedIndex = 0;
+  var pages = [
+    const PageOne(),
+    const PageTwo(),
+    const PageThree(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Bottom Navigation Bar Project")),
+      body: pages[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.looks_one_outlined), label: "One"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.looks_two_outlined), label: "Two"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.looks_3_outlined), label: "Three"),
+        ],
+        currentIndex: selectedIndex,
+        backgroundColor: Colors.lightGreen,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.white,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
