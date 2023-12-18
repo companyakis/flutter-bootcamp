fb97758883c46e827c15da8949887847f8d91daf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-18 09:49:01 +0300

    Create Day6-Navigator2.dart

---
+++ Day6-Navigator2.dart
@@ -0,0 +1,43 @@
+//game_screen.dart file
+
+import 'package:flutter/material.dart';
+import 'package:sixthday/result_screen.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Game Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("This is game page"),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ResultScreen()));
+              },
+              child: const Text("Result Page"),
+            ),
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
