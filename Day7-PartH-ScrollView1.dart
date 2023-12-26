ca3220a8e9113fe02ecf38dcd1a4798219ce0737
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 11:00:06 +0300

    Create Day7-PartH-ScrollView1.dart

---
+++ Day7-PartH-ScrollView1.dart
@@ -0,0 +1,73 @@
+//widgets.dart file
+
+//main.dart file is different
+
+import 'package:flutter/material.dart';
+
+class Widgets extends StatefulWidget {
+  const Widgets({super.key});
+
+  @override
+  State<Widgets> createState() => _WidgetsState();
+}
+
+class _WidgetsState extends State<Widgets> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ScrollView")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.black,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blueGrey,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.purple,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.black,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
