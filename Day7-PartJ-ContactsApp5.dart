fff1dfc0dedf194993e27b5914b6f65f50ff08bd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 14:36:15 +0300

    Create Day7-PartJ-ContactsApp5.dart

---
+++ Day7-PartJ-ContactsApp5.dart
@@ -0,0 +1,12 @@
+//people.dart file
+
+class People {
+  int personID;
+  String personName;
+  String personMobile;
+
+  People(
+      {required this.personID,
+      required this.personName,
+      required this.personMobile});
+}
